60e090a82e04f39a8b998096024138db5e49cc44
Axel Jeremy Solano <axeljeremy@bernhard133-014.wireless.wmich.edu>
2018-04-14 14:21:02 -0400

    google button

--- php/calendar.php
+++ php/calendar.php
@@ -150,6 +150,19 @@ session_start();
 </div><!--  End of Modal -->
 
 
+<div class="fixed-action-btn">
+  <a class="btn-floating btn-large red">
+    <i class="large material-icons">mode_edit</i>
+  </a>
+  <ul>
+    <li><a class="btn-floating red"><i class="material-icons">insert_chart</i></a></li>
+    <li><a class="btn-floating yellow darken-1"><i class="material-icons">format_quote</i></a></li>
+    <li><a class="btn-floating green"><i class="material-icons">publish</i></a></li>
+    <li><a class="btn-floating blue"><i class="material-icons">attach_file</i></a></li>
+  </ul>
+</div>
+
+
 
 
 <!-- Footer -->
@@ -158,6 +171,14 @@ session_start();
 
 <script type="text/JavaScript">
   $(document).ready(function() {
+
+    //var buttonDown = document.querySelector('.fixed-action-btn');
+    //var instanceDown = M.FloatingActionButton.init(elem,options);
+    //var instance = M.FloatingActionButton.getInstance(elem);
+    //instanceDown.open();
+
+     $('.fixed-action-btn').floatingActionButton();
+     $('.fixed-action-btn').floatingActionButton('open');
 
     /// PART CALENDAR LEFT ###########################################################
     $('#calendar-left').fullCalendar({
